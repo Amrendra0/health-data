55d78c956cc3a2d264647a59b459c1685518a267
Amrendra0 <76563910+Amrendra0@users.noreply.github.com>
2024-05-21 20:50:00 +0530

    Delete target directory

--- target/debug/deps/healthdata-358b1d5e7c88dec0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\USER\OneDrive\Desktop\github\bansal group\healthdata\target\debug\deps\libhealthdata-358b1d5e7c88dec0.rmeta: src\main.rs
-
-C:\Users\USER\OneDrive\Desktop\github\bansal group\healthdata\target\debug\deps\healthdata-358b1d5e7c88dec0.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/healthdata-d64bb72420c30c3f.d
+++
@@ -1,5 +0,0 @@
-C:\Users\USER\OneDrive\Desktop\github\bansal group\healthdata\target\debug\deps\libhealthdata-d64bb72420c30c3f.rmeta: src\main.rs
-
-C:\Users\USER\OneDrive\Desktop\github\bansal group\healthdata\target\debug\deps\healthdata-d64bb72420c30c3f.d: src\main.rs
-
-src\main.rs:
